c99fbd996de7454b699030599af768e3dec245dd
Solomon <solomonlazio@gmail.com>
2019-01-12 02:12:05 +0000

    updated todos

---
+++ version_7.js
@@ -0,0 +1,8 @@
+//Practical JavaScript - Version 7
+
+// V7 Requirements:
+/*
+ *.toggleAll: if everything is true, make everything false
+ *.toggleAll: Otherwise, make everything true
+ *.displayTodos should show .completed
+ */
