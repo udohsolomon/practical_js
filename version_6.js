934744dec9e716671ec469b3594ca93c423d746e
Solomon <solomonlazio@gmail.com>
2019-01-12 00:50:55 +0000

    updated todos

---
+++ version_6.js
@@ -0,0 +1,71 @@
+//Practical JavaScript - Version 6
+
+// V6 Requirements:
+/*
+ *.toggleAll: if everything is true, make everything false
+ *.toggleAll: Otherwise, make everything true
+ *.displayTodos should show .completed
+ */
+
+var todoList = {
+    todos: [],
+    displayTodos: function() {
+        if (this.todos.length === 0) {
+            console.log('Your todo list is empty!')
+        }
+        else {
+            console.log('My Todos: ') //this.todos not super useful, so we can do wo it.
+            for ( var i = 0; i < this.todos.length; i++) {
+                if (this.todos[i].completed === true) {
+                    console.log('[✔]', this.todos[i].todoText)
+                }
+                else {
+                    console.log('[ ]', this.todos[i].todoText)
+                }
+            }
+        }
+
+    },
+    addTodos: function(todoText) {
+        this.todos.push({
+            todoText: todoText,
+            completed: false
+        });
+        this.displayTodos() 
+        //todoList.addTodos('This is a todo')
+    },
+    changeTodos: function(position, todoText) {
+        this.todos[position].todoText = todoText
+        this.displayTodos()
+    },
+    /* toggleTodos: function(position) {
+     * var todo = this.todos[position]
+     * todo.completed = !todo.completed
+     * this.displayTodos()
+     * },
+     */ 
+    toggleAll: function() {
+        var totalTodos = this.todos.length
+        var completedTodos = 0
+        // get number of completed todos
+        for (var i; i < totalTodos; i++) {
+            if (this.todos[i].completed === true) {
+                completed++
+            }
+        }
+        // case 1: If everything is true, make everything false 
+        if (completedTodos === totalTodos) {
+            for (var i = 0; i < totalTodos; i++) {
+                this.todos[i].completed === false
+            }
+        }   
+    } 
+        
+        
+        
+    },
+    deleteTodos: function(position) {
+        this.todos.splice(position, 1)
+        this.displayTodos()
+    }
+} 
