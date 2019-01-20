5479c63a37a9dce9350adf19e924f2fc81b206e7
Solomon <solomonlazio@gmail.com>
2019-01-20 00:30:40 +0000

    add version 9

--- version_9.js
+++ version_9.js
@@ -9,23 +9,6 @@
 
 var todoList = {
     todos: [],
-    displayTodos: function() {
-        if (this.todos.length === 0) {
-            console.log('Your todo list is empty!')
-        }
-        else {
-            console.log('My Todos: ') //this.todos is not super useful, so we can do w/o it.
-            for ( var i = 0; i < this.todos.length; i++) {
-                if (this.todos[i].completed === true) {
-                    console.log('[✔]', this.todos[i].todoText)
-                }
-                else {
-                    console.log('[ ]', this.todos[i].todoText)
-                }
-            }
-        }
-
-    },
     addTodos: function(todoText) {
         this.todos.push({
             todoText: todoText,
@@ -36,13 +19,11 @@ var todoList = {
     },
     changeTodos: function(position, todoText) {
         this.todos[position].todoText = todoText
-        this.displayTodos()
     },
 
     toggleCompleted: function(position) {
      var todo = this.todos[position]
      todo.completed = !todo.completed
-     this.displayTodos()
      },
  
     toggleAll: function() {
@@ -67,18 +48,13 @@ var todoList = {
 
             }
         }
-        this.displayTodos()   
     }, 
     deleteTodos: function(position) {
         this.todos.splice(position, 1)
-        this.displayTodos()
     }
 } 
 
 var handlers = {
-    displayTodosButton: function() {
-        todoList.displayTodos()
-    },
     addTodos: function() {
         var addTodosTextInput = document.getElementById('addTodosTextInput')
         todoList.addTodos(addTodosTextInput.value)
